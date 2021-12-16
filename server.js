a6cef5ede60302521a4596c34777e2ae7849829c
Naman Vinod <namanvinod1@gmail.com>
2021-12-16 14:42:47 +0530

    Adding Register

--- server.js
+++ server.js
@@ -29,10 +29,36 @@ App.post('/api/login', async (req, res) => {
     const token = jwt.sign(
         { userName },
         'DNRJDed44Dcc*ff',
-        { expiresIn: 30 }
+        { expiresIn: '2h' }
     );
 
     res.status(201).send ({ userName, token });
+});
+
+App.post('/api/register', async (req, res) => {
+    const { userName, password } = req.body;
+
+    if(!userName || !password) return res.status(400).send('Invalid Inputs');
+
+    if(!db || !db.users || !db.users.length) return res.status(500).send('ERROR');
+
+    const user = db.users.find(user => user.userName === userName);
+
+    if(user) return res.status(200).send('Already Registered');
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    console.log(encryptedPassword);
+
+    db.addUsers([{ userName, password: encryptedPassword, isActive: true }]);
+
+    const token = jwt.sign(
+        { userName },
+        'DNRJDed44Dcc*ff',
+        { expiresIn: "2h" }
+    );
+
+    res.status(201).json({ userName, token });
 });
 
 App.post('/api/welcome', auth, (req, res) => {
