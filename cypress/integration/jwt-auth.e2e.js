2650293a952989941dfd6d0410ce62fd2c0b65df
Naman Vinod <namanvinod1@gmail.com>
2021-12-22 12:02:03 +0530

    Adding Get All Users Test

--- cypress/integration/jwt-auth.e2e.js
+++ cypress/integration/jwt-auth.e2e.js
@@ -6,5 +6,14 @@ describe('Get User Flow', () => {
             expect(response.body).to.have.length(1);
             expect(response.body[0]).to.have.property('userName', 'user1');
         });
-    })
+    });
+
+    it('Get Users', () => {
+        cy.request('http://localhost:1000/api/users')
+        .then(response => {
+            expect(response).to.have.property('body');
+            expect(response.body).to.have.length(2);
+            expect(response.body[0]).to.have.property('userName', 'user1');
+        });
+    });
 });
