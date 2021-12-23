f01a22daf2799a3856d74d6918215f3ad356af52
Naman Vinod <namanvinod1@gmail.com>
2021-12-23 21:28:06 +0530

    Updated DB

--- server.js
+++ server.js
@@ -32,13 +32,13 @@ App.post('/api/login', async (req, res) => {
     const user = users.find(user => user.userName === userName);
     if(!user) return res.status(200).send('Unregistered User');
 
-    const userValid = await bcrypt.compare(password, user.password);
+    const userValid = await bcrypt.compare(password, user.encryptedPassword);
 
     if(!userValid) res.status(400).send('Invalid User Credentials');
     
     const token = jwt.sign(
         { userName },
-        'DNRJDed44Dcc*ff',
+        process.env.JWTKEY,
         { expiresIn: '2h' }
     );
 
@@ -50,25 +50,23 @@ App.post('/api/register', async (req, res) => {
 
     if(!userName || !password) return res.status(400).send('Invalid Inputs');
 
-    if(!db || !db.users || !db.users.length) return res.status(500).send('ERROR');
+    if(!db) return res.status(500).send('ERROR');
 
-    const user = db.users.find(user => user.userName === userName);
+    const user = db?.users?.find(user => user.userName === userName);
 
     if(user) return res.status(200).send('Already Registered');
 
     const encryptedPassword = await bcrypt.hash(password, 10);
 
-    console.log(encryptedPassword);
-
-    db.addUsers([{ userName, password: encryptedPassword, isActive: true }]);
+    db.addUsers([{ userName, password, encryptedPassword, isActive: true }]);
 
     const token = jwt.sign(
         { userName },
-        'DNRJDed44Dcc*ff',
-        { expiresIn: "2h" }
+        process.env.JWTKEY,
+        { expiresIn: process.env.JWTEXPIRY }
     );
 
-    res.status(201).json({ userName, token });
+    res.status(201).send('User Created');
 });
 
 App.post('/api/welcome', auth, (req, res) => {
@@ -76,7 +74,7 @@ App.post('/api/welcome', auth, (req, res) => {
 });
 
 App.get('/api/users', (req, res) => {
-    return res.status(200).send(db.getAllUsers());
+    return res.status(200).send(getAllUsers());
 });
 
 App.get('/api/users/:username', (req, res) => {
